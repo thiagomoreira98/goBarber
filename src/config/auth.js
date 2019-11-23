8c001fd5957299fb38f013ee3dc0560abf8b2052
Thiago Moreira <moreira.g.thiago@gmail.com>
2019-11-23 00:52:07 -0300

    fase 1

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0afd767c1c86c6103bae43dcc7f1286a',
+  expiresInt: '7d',
+};
---
+++ src/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  username: 'postgres',
+  password: '1234',
+  database: 'gobarber',
+  host: '127.0.0.1',
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
